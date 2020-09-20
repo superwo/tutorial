793325e169f23a6588a88c4f8b3c7ed9238c723d
Radu <avadanii@tiscali.it>
2020-09-20 20:27:55 +0300

    added some pages

---
+++ src/pages/blog.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const blog = () => {
+  return <h1>this is our blog page</h1>
+}
+
+export default blog
---
+++ src/pages/products.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const products = () => {
+  return <h1>This is products page</h1>
+}
+
+export default products
