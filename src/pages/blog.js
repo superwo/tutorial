ce6f84d7936ab30efd38a204bf18a33bf7de3cd4
Radu <avadanii@tiscali.it>
2020-09-22 21:02:07 +0300

    navbar

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,13 @@
+import { Link } from "gatsby"
 import React from "react"
 
 const blog = () => {
-  return <h1>this is our blog page</h1>
+  return (
+    <div>
+      <h1>this is our blog page</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
 }
 
 export default blog
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
 import React from "react"
+import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello people!</div>
+  return (
+    <div>
+      <Navbar />
+      Hello people!
+      <div>
+        <Link to="/blog/">blog page</Link>
+      </div>
+      <div>
+        <a href="https//www.gatsbyjs.org">gatsby docs</a>
+      </div>
+    </div>
+  )
 }
